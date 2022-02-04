7e23e1348c5f0f0025277d67d9be40a5437b97b2
rodan13 <rodrigohernandezb7@gmail.com>
2022-02-03 20:50:31 -0600

    06-modelos-estadisticos uploaded

---
+++ R/01-notas.R
@@ -0,0 +1,30 @@
+# Usethis ayuda a interarctuar con los archivos para configurar r, como para usar el archivo r profile, usethis abre ese archivo.
+#
+# Ctrl + a -> para seleccionar todo
+#
+# Ctrl + Shift + C -> para comentar todo lo que esta seleccionado.
+#
+# Alt + click izquierdo del raton -> genera varios cursores, para poder seleccionar multiples renglones de un archivo. Con la tecla End para ir al final de cada uno de los renglones seleccionados.
+#
+# Ctrl + Alt + click izquierdo en los renglones que desee -> para crear cursores en los renglones que yo quiera y en las posiciones donde quiera.
+#
+# here nos ayuda a escribir codigo que sea compartible.
+
+library("usethis")
+library("here")
+library("biocthis")
+library("lobstr")
+library("postcards")
+library("sessioninfo")
+library("SummarizedExperiment")
+library("iSEE")
+library("edgeR")
+library("ExploreModelMatrix")
+library("limma")
+library("recount3")
+library("pheatmap")
+library("ggplot2")
+library("patchwork")
+library("RColorBrewer")
+library("spatialLIBD")
+library("Magick++")
